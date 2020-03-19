febe47e2097c1cc4ec62dc630910e5551ca2b634
Austen Bright <pureaustenbright@gmail.com>
2020-03-19 21:11:51 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,18 +13,18 @@
 #     }
 #   }
 # }
-# 
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
@@ -50,7 +50,7 @@ def naming_system
        sub_category: {
          label: "Order",
          sub_category: {
-           label: "Family", 
+           label: "Family",
            sub_category: {
              label: "Genus",
              sub_category: {
